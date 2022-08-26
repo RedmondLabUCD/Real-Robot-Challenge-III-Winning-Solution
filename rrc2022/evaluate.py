833c40c64465e74a7f273db22a504ef8dfa0aaab
Qiang Wang <77690005+wq1448878756@users.noreply.github.com>
2022-08-26 20:50:39 +0100

    Update evaluate.py

--- rrc2022/evaluate.py
+++ rrc2022/evaluate.py
@@ -10,8 +10,8 @@ from d3rlpy.algos import BC as algo
 import d3rlpy
 from . import policies
 
-model_name = 'model_8261630.pt'
-json_name = 'params_8261630.json'
+model_name = 'model_8262049.pt'
+json_name = 'params_8262049.json'
 
 class TorchBasePolicy(PolicyBase):
     def __init__(
