f661a2f8fe1fcbc6d53d488f197e431ed720270c
Qiang Wang <73186518+wq13552463699@users.noreply.github.com>
2022-09-16 10:35:58 +0100

    Update evaluate_rnnbc.py

--- rrc2022/evaluate_rnnbc.py
+++ rrc2022/evaluate_rnnbc.py
@@ -80,7 +80,8 @@ class TorchBasePolicy(PolicyBase):
             self.policy.eval()
             observation = torch.Tensor([observation]).to(torch.float32)
             #t1 = time.time()
-            action, self.h = self.policy(observation, self.h).cpu().detach().numpy()[0]
+            action, self.h = self.policy(observation, self.h)
+            action = action.cpu().detach().numpy()[0]
             #print(time.time() - t1)
             return action
 
