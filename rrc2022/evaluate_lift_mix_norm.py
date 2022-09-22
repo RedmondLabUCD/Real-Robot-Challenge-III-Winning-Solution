cd65d4aaf7bbd5477a6943a37fead3e8f98e951a
Qiang Wang <73186518+wq13552463699@users.noreply.github.com>
2022-09-22 16:40:43 +0100

    Update evaluate_lift_mix_norm.py

--- rrc2022/evaluate_lift_mix_norm.py
+++ rrc2022/evaluate_lift_mix_norm.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import numpy as np
 
 ############################
-model_name = 'ckpt_2_2_augtunenorm.pth'
+model_name = 'lift_mix_aug_tune_norm.pth'
 norm_name = 'lift_mix_norm.npy'
 norm_mode = 'std'
 ############################
