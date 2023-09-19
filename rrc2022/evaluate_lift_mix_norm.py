a25707f7c60ec4f52cbb170e41fbe99142dbd158
Qiang Wang <73186518+wq13552463699@users.noreply.github.com>
2023-09-19 22:27:31 +0100

    Update evaluate_lift_mix_norm.py

--- rrc2022/evaluate_lift_mix_norm.py
+++ rrc2022/evaluate_lift_mix_norm.py
@@ -7,7 +7,7 @@ import numpy as np
 ############################
 #model_name = 'lift_mix_aug_tune_norm.pth'
 #norm_name = 'lift_mix_norm.npy'
-model_name = 'lift_mix_filter_aug_tune_norm.pth'
+model_name = 'model1.pth'
 norm_name = 'lift_mix_filter_norm.npy'
 norm_mode = 'std'
 ############################
