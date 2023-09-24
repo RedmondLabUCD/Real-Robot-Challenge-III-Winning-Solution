1954d7de5a00f127097b530796530481730ee9cf
Qiang Wang <73186518+wq13552463699@users.noreply.github.com>
2023-09-24 23:28:56 +0100

    Update evaluate_lift_mix_norm.py

--- rrc2022/evaluate_lift_mix_norm.py
+++ rrc2022/evaluate_lift_mix_norm.py
@@ -5,10 +5,10 @@ import torch.nn as nn
 import numpy as np
 
 ############################
-#model_name = 'lift_mix_aug_tune_norm.pth'
-#norm_name = 'lift_mix_norm.npy'
-model_name = 'model_q3.pth'
-norm_name = 'params_q.npy'
+model_name = 'lift_mix_aug_tune_norm.pth'
+norm_name = 'lift_mix_norm.npy'
+#model_name = 'model_q3.pth'
+#norm_name = 'params_q.npy'
 norm_mode = 'std'
 ############################
 
